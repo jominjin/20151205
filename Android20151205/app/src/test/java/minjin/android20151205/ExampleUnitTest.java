dec73b2799f26b634086029e1d61578aeafed02d
jominjin <jominjin95@naver.com>
2015-12-05 01:29:50 +0900

    Create new project
    Create new project in androidStudio, 20151205

---
+++ Android20151205/app/src/test/java/minjin/android20151205/ExampleUnitTest.java
@@ -0,0 +1,15 @@
+package minjin.android20151205;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+/**
+ * To work on unit tests, switch the Test Artifact in the Build Variants view.
+ */
+public class ExampleUnitTest {
+    @Test
+    public void addition_isCorrect() throws Exception {
+        assertEquals(4, 2 + 2);
+    }
+}
